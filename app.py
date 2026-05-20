63652d737246a85f2f2862b0a04414bf8a1a6683
Honnasan <127916992+Honnasan@users.noreply.github.com>
2026-05-20 15:10:36 +0700

    Update app.py

--- app.py
+++ app.py
@@ -18,7 +18,10 @@ with open('label_map.json', 'r') as f:
 
 # ======= Load Model =======
 model = models.mobilenet_v3_large(pretrained=False)
-model.classifier[1] = torch.nn.Linear(model.last_channel, 102)
+
+in_features = model.classifier[3].in_features
+model.classifier[3] = torch.nn.Linear(in_features, 102)
+
 model.load_state_dict(torch.load(model_path, map_location=device))
 model = model.to(device)
 model.eval()
